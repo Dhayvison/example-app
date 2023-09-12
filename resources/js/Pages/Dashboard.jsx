96e600b4c90fbca0f3634fc9db607f88e36df37a
Dhayvison Braga <dhayvisonbraga@hotmail.com>
2023-09-12 20:32:22 -0300

    remove dolar currence component

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,23 +1,9 @@
-import Container from "@/Components/Container";
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
-import { useEffect, useState } from "react";
-import { useAsync, useFetch } from "react-async";
-
-const loadDolarData = () =>
-    fetch(
-        "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/gusd/brl.json"
-    ).then((res) => res.json());
+import Container from "@/Components/Container";
+import Paper from "@/Components/Paper";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 
 export default function Dashboard({ auth }) {
-    const { data, error, isPending, run } = useAsync({
-        promiseFn: loadDolarData,
-    });
-
-    useEffect(() => {
-        run();
-    });
-
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -32,38 +18,7 @@ export default function Dashboard({ auth }) {
             <div className="py-12">
                 <Container>
                     <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-                        <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                            <div className="p-6 text-gray-900 dark:text-gray-100">
-                                You're logged in!
-                            </div>
-                        </div>
-
-                        <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                            <div className="p-6">
-                                <div className="flex justify-between">
-                                    <small>1 US dollar is equal to</small>
-                                    {isPending ? (
-                                        <small className="animate-pulse w-1/3 h-4 bg-slate-700 rounded"></small>
-                                    ) : (
-                                        <small>
-                                            {new Date(
-                                                data.date
-                                            ).toLocaleDateString()}
-                                        </small>
-                                    )}
-                                </div>
-                                <div className="text-4xl">
-                                    {isPending ? (
-                                        <div className="animate-pulse w-1/3 h-10 bg-slate-700 rounded"></div>
-                                    ) : (
-                                        new Intl.NumberFormat("pt-BR", {
-                                            style: "currency",
-                                            currency: "BRL",
-                                        }).format(data.brl)
-                                    )}
-                                </div>
-                            </div>
-                        </div>
+                        <Paper>You're logged in!</Paper>
                     </div>
                 </Container>
             </div>
